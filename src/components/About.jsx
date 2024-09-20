22557f2caf76f8246a829c68b0de58815c57873c
thegaab <tiagogabrielcontato@hotmail.com>
2024-09-20 11:16:11 -0300

    new profile update, fix size image

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -13,7 +13,7 @@ const About = () => {
                 transition={{duration: 0.5}}
                 className="w-full lg:w-1/2 lg:p-8">
                     <div className="flex items-center justify-center">
-                        <img className="rounded-2xl" src={aboutImg} alt="Tiago Gabriel" />
+                        <img className="rounded-2xl lg:w-1/2" src={aboutImg} alt="Tiago Gabriel" />
                     </div>
             </motion.div>
             <motion.div
